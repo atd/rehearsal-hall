4e9bb20691e2986af5197c91da5489ee03cd0a64
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2012-11-24 13:39:14 +0100

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RehearsalHall::Application.config.secret_token = '8f1605b2821343d200c13c9041fa862c9f0baf566239aa6d7b8459f91d78aa3aefe38dfcbe3f11dd9aa4721247dda4d700c32ee863b1a5b75d2da200624cf3af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RehearsalHall::Application.config.session_store :cookie_store, key: '_rehearsal-hall_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RehearsalHall::Application.config.session_store :active_record_store
